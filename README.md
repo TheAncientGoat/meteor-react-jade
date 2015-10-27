53be8a5238c06b6d99e51ada0f4325c86ce0d283
Ryan Swart <ryanswrt@gmail.com>
2015-10-28 01:23:10 +0800

    modify readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Jade React Templates (for Meteor)
+
+Based on https://github.com/jadejs/react-jade
+
+Currently takes all jade files and turns them into jade.js files, stored as global functions named after their filenames
+
+Still experimental, api will most likely change in the future
--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'jade-react',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Compile Jade files for use in React elements',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/TheAncientGoat/meteor-react-jade',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
